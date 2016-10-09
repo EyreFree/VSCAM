70afaf0144423dd9c1371ded0c2cdcf8cbdd2d07
EyreFree <eyrefree@eyrefree.org>
2016-10-09 17:56:56 +0800

    添加LaunchImage

--- VSCAM/AppDelegate.swift
+++ VSCAM/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        showStatusBar()
         return true
     }
 
@@ -30,17 +30,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        showStatusBar()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        showStatusBar()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    //自定义
+    //在plist中关闭状态栏然后进app后打开可以做到loading全屏无状态栏
+    func showStatusBar() {
+        if UIApplication.shared.isStatusBarHidden {
+            UIApplication.shared.isStatusBarHidden = false
+        }
+    }
 }
 
