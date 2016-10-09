ac0b736dcd7233585181e67d3dafda6c7fc1ecbd
EyreFree <eyrefree@eyrefree.org>
2016-10-09 17:35:59 +0800

    Initial Commit

---
+++ VSCAM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VSCAM
+//
+//  Created by EyreFreeWork on 16/10/9.
+//  Copyright © 2016年 EyreFree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
