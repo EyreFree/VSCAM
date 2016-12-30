d56ad46653b6eac3a094cd0ef22dc49289f4d7b3
EyreFree <eyrefree@EyreFreedeMacBook-Pro.local>
2016-12-30 17:04:25 +0800

    运算符重载

--- VSCAM/Operator+.swift
+++ VSCAM/Operator+.swift
@@ -2,4 +2,11 @@
 
 import Foundation
 
+precedencegroup PowerPrecedence {
+    higherThan: MultiplicationPrecedence
+}
 
+infix operator ^^ : PowerPrecedence
+func ^^ (radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
+}
